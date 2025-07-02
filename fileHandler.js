ff66a5a751d797cb3c469404380a45b6c8e7f6eb
nsrksreecharan <75178502+nsrksreecharan@users.noreply.github.com>
2025-07-02 12:27:00 +0530

    Update fileHandler.js

--- fileHandler.js
+++ fileHandler.js
@@ -1,16 +1,49 @@
-const fs=require('fs');
-const path=require('path');
+const fs=require("fs");
+const fsp=fs.promises;
+const path=require("path");
 
-const filePath=path.join(__dirname,'./notes.json');
+const filePath=path.join(__dirname,"./notes.json");
 
-function readNotes(){
-    console.log(filePath);
-    const data=fs.readFileSync(filePath,'utf-8') || "";
-    return JSON.parse(data);
+const readNotes=()=>{
+    if (!fs.existsSync(filePath)) {
+        return []; 
+    }
+    const notes=fs.readFileSync(filePath,"utf-8") || "";
+    if (!notes) {
+        return []; // return empty array if file is empty
+    }
+
+    try {
+        return JSON.parse(notes);
+    } catch (err) {
+        console.error("Invalid JSON format in notes.json:", err.message);
+        return [];
+    }
 }
 
-function writeNotes(notes){
+const writeNotes=(notes)=>{
     fs.writeFileSync(filePath,JSON.stringify(notes,null,2));
 }
 
-module.exports ={readNotes,writeNotes};
+const readNotesAsync=async()=>{
+    try{
+        await fsp.access(filePath);
+        const notes=await fsp.readFile(filePath,"utf-8")||"";
+        if(!notes) return [];
+
+        return JSON.parse(notes);
+    } catch (err){
+        console.error("Error with async file reading",err)
+        return [];
+    }
+}
+
+const writeNotesAsync=async(notes)=>{
+    try{
+        await fsp.writeFile(notes);
+    } catch(err){
+        console.log("Can't write file error",err);
+    }
+};
+
+module.exports={readNotes,writeNotes,readNotesAsync,writeNotesAsync};
